3f6fc8ab887aa29823f0fdb2e003118f7ed5b015
nutant <65939836+nutant233@users.noreply.github.com>
2026-05-16 17:44:04 +0800

    1.20

--- src/main/java/fast/fasttag/FastTag.java
+++ src/main/java/fast/fasttag/FastTag.java
@@ -1,11 +1,11 @@
 package fast.fasttag;
 
 import com.google.common.collect.MapMaker;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.neoforged.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod;
 
 import java.util.function.Function;
 
@@ -13,31 +13,31 @@ import java.util.function.Function;
 @SuppressWarnings("rawtypes")
 public class FastTag {
 
-    private static final Function<ResourceKey, MapCache<Identifier, TagKey>> TAG_MAP_FUNCTION = k -> MapCache.build(tagFunction(k)).maker(MapMaker::weakValues).build();
-    private static final MapCache<ResourceKey, MapCache<Identifier, TagKey>> TAG_INTERNING_MAP = MapCache.build(TAG_MAP_FUNCTION).build();
+    private static final Function<ResourceKey, MapCache<ResourceLocation, TagKey>> TAG_MAP_FUNCTION = k -> MapCache.build(tagFunction(k)).maker(MapMaker::weakValues).build();
+    private static final MapCache<ResourceKey, MapCache<ResourceLocation, TagKey>> TAG_INTERNING_MAP = MapCache.build(TAG_MAP_FUNCTION).build();
 
-    private static Function<Identifier, TagKey> tagFunction(ResourceKey key) {
+    private static Function<ResourceLocation, TagKey> tagFunction(ResourceKey key) {
         return id -> new TagKey(key, id);
     }
 
-    public static MapCache<Identifier, TagKey> getTagCache(ResourceKey<?> resourceKey) {
+    public static MapCache<ResourceLocation, TagKey> getTagCache(ResourceKey<?> resourceKey) {
         return TAG_INTERNING_MAP.getCache(resourceKey);
     }
 
-    private static final Function<ResourceKey, MapCache<Identifier, ResourceKey>> RESOURCEKEY_MAP_FUNCTION = k -> MapCache.build(resourcekeyFunction(k)).maker(MapMaker::weakValues).build();
-    private static final MapCache<ResourceKey, MapCache<Identifier, ResourceKey>> RESOURCEKEY_INTERNING_MAP = MapCache.build(RESOURCEKEY_MAP_FUNCTION).build();
-    private static final ResourceKey ROOT = new ResourceKey(Registries.ROOT_REGISTRY_NAME, Registries.ROOT_REGISTRY_NAME);
-    private static final MapCache<Identifier, ResourceKey> ROOT_REGISTRY_MAP = RESOURCEKEY_INTERNING_MAP.getCache(ROOT);
+    private static final Function<ResourceKey, MapCache<ResourceLocation, ResourceKey>> RESOURCEKEY_MAP_FUNCTION = k -> MapCache.build(resourcekeyFunction(k)).maker(MapMaker::weakValues).build();
+    private static final MapCache<ResourceKey, MapCache<ResourceLocation, ResourceKey>> RESOURCEKEY_INTERNING_MAP = MapCache.build(RESOURCEKEY_MAP_FUNCTION).build();
+    private static final ResourceKey ROOT = new ResourceKey(BuiltInRegistries.ROOT_REGISTRY_NAME, BuiltInRegistries.ROOT_REGISTRY_NAME);
+    private static final MapCache<ResourceLocation, ResourceKey> ROOT_REGISTRY_MAP = RESOURCEKEY_INTERNING_MAP.getCache(ROOT);
 
-    private static Function<Identifier, ResourceKey> resourcekeyFunction(ResourceKey key) {
-        return id -> new ResourceKey(key.identifier(), id);
+    private static Function<ResourceLocation, ResourceKey> resourcekeyFunction(ResourceKey key) {
+        return id -> new ResourceKey(key.location(), id);
     }
 
-    public static MapCache<Identifier, ResourceKey> getResourceKeyCache(ResourceKey<?> resourceKey) {
+    public static MapCache<ResourceLocation, ResourceKey> getResourceKeyCache(ResourceKey<?> resourceKey) {
         return RESOURCEKEY_INTERNING_MAP.getCache(resourceKey);
     }
 
-    public static ResourceKey getRootResourceKey(Identifier identifier) {
+    public static ResourceKey getRootResourceKey(ResourceLocation identifier) {
         return ROOT_REGISTRY_MAP.getCache(identifier);
     }
 
--- src/main/java/fast/fasttag/mixin/ResourceKeyMixin.java
+++ src/main/java/fast/fasttag/mixin/ResourceKeyMixin.java
@@ -3,9 +3,8 @@ package fast.fasttag.mixin;
 import com.mojang.serialization.Codec;
 import fast.fasttag.FastTag;
 import net.minecraft.core.Registry;
-import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -20,25 +19,16 @@ public class ResourceKeyMixin {
     @Overwrite
     public static <T> Codec codec(ResourceKey<? extends Registry<T>> registryName) {
         var cache = FastTag.getResourceKeyCache(registryName);
-        return Identifier.CODEC.xmap(cache::getCache, ResourceKey::identifier);
+        return ResourceLocation.CODEC.xmap(cache::getCache, ResourceKey::location);
     }
+
 
     /**
      * @author nutant233
      * @reason optimize
      */
     @Overwrite
-    public static <T> StreamCodec streamCodec(ResourceKey<? extends Registry<T>> registryName) {
-        var cache = FastTag.getResourceKeyCache(registryName);
-        return Identifier.STREAM_CODEC.map(cache::getCache, ResourceKey::identifier);
-    }
-
-    /**
-     * @author nutant233
-     * @reason optimize
-     */
-    @Overwrite
-    public static <T> ResourceKey create(ResourceKey<? extends Registry<T>> registryName, Identifier location) {
+    public static <T> ResourceKey create(ResourceKey<? extends Registry<T>> registryName, ResourceLocation location) {
         return FastTag.getResourceKeyCache(registryName).getCache(location);
     }
 
@@ -47,7 +37,7 @@ public class ResourceKeyMixin {
      * @reason optimize
      */
     @Overwrite
-    public static ResourceKey createRegistryKey(Identifier identifier) {
+    public static ResourceKey createRegistryKey(ResourceLocation identifier) {
         return FastTag.getRootResourceKey(identifier);
     }
 
@@ -56,7 +46,7 @@ public class ResourceKeyMixin {
      * @reason deprecated
      */
     @Overwrite
-    private static <T> ResourceKey<T> create(Identifier registryName, Identifier identifier) {
+    private static <T> ResourceKey<T> create(ResourceLocation registryName, ResourceLocation identifier) {
         throw new UnsupportedOperationException("[FastTag] This method is deprecated");
     }
 }
